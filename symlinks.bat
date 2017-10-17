9a19102f5d95e052777c7bcef00e051e6c4ae3b3
darkoverlordofdata <darkoverlordofdata@gmail.com>
2017-10-16 21:28:46 -0700

    refactor

--- symlinks.bat
+++ symlinks.bat
@@ -4,5 +4,4 @@ mklink /D C:\Users\darko\Documents\GitHub\zerog-match3\src\entitas C:\Users\dark
 mklink /D C:\Users\darko\Documents\GitHub\zerog-match3\src\sdx C:\Users\darko\Documents\GitHub\zerog\lib\sdx
 mklink /D C:\Users\darko\Documents\GitHub\zerog-match3\src\vapis C:\Users\darko\Documents\GitHub\zerog\lib\vapis
 mklink /D C:\Users\darko\Documents\GitHub\zerog-match3\src\vapis-em C:\Users\darko\Documents\GitHub\zerog\lib\vapis-em
-mklink C:\Users\darko\Documents\GitHub\zerog-match3\src\mt19937ar.c C:\Users\darko\Documents\GitHub\zerog\lib\mt19937ar.c
-mklink C:\Users\darko\Documents\GitHub\zerog-match3\src\mt19937ar.h C:\Users\darko\Documents\GitHub\zerog\lib\mt19937ar.h
+mklink /D C:\Users\darko\Documents\GitHub\zerog-match3\src\mt19937 C:\Users\darko\Documents\GitHub\zerog\lib\mt19937
--- symlinks.sh
+++ symlinks.sh
@@ -5,6 +5,5 @@ ln -s /home/bruce/Applications/zerog/lib/entitas src/entitas
 ln -s /home/bruce/Applications/zerog/lib/sdx src/sdx
 ln -s /home/bruce/Applications/zerog/lib/vapis src/vapis
 ln -s /home/bruce/Applications/zerog/lib/vapis-em src/vapis-em
-ln -s /home/bruce/Applications/zerog/lib/mt19937ar.c src/mt19937ar.c
-ln -s /home/bruce/Applications/zerog/lib/mt19937ar.h src/mt19937ar.h
+ln -s /home/bruce/Applications/zerog/lib/mt19937 src/mt19937
 
