c5f58127f9405ec388f089bab011a77c35b1ffac
Bruce Davidson <darkoverlordofdata@gmail.com>
2017-10-16 18:59:26 -0700

    sync windows 10

--- symlinks.bat
+++ symlinks.bat
@@ -1,8 +1,8 @@
 
-mklink /D C:\Users\darko\Documents\GitHub\zerog-match3\src\goop C:\msys64\mingw64\share\zerog\lib\goop 
-mklink /D C:\Users\darko\Documents\GitHub\zerog-match3\src\entitas C:\msys64\mingw64\share\zerog\lib\entitas
-mklink /D C:\Users\darko\Documents\GitHub\zerog-match3\src\sdx C:\msys64\mingw64\share\zerog\lib\sdx
-mklink /D C:\Users\darko\Documents\GitHub\zerog-match3\src\vapis C:\msys64\mingw64\share\zerog\lib\vapis
-mklink /D C:\Users\darko\Documents\GitHub\zerog-match3\src\vapis-em C:\msys64\mingw64\share\zerog\lib\vapis-em
-mklink C:\Users\darko\Documents\GitHub\zerog-match3\src\mt19937ar.c C:\msys64\mingw64\share\zerog\lib\mt19937ar.c
-mklink C:\Users\darko\Documents\GitHub\zerog-match3\src\mt19937ar.h C:\msys64\mingw64\share\zerog\lib\mt19937ar.h
+mklink /D C:\Users\darko\Documents\GitHub\zerog-match3\src\goop C:\Users\darko\Documents\GitHub\zerog\lib\goop 
+mklink /D C:\Users\darko\Documents\GitHub\zerog-match3\src\entitas C:\Users\darko\Documents\GitHub\zerog\lib\entitas
+mklink /D C:\Users\darko\Documents\GitHub\zerog-match3\src\sdx C:\Users\darko\Documents\GitHub\zerog\lib\sdx
+mklink /D C:\Users\darko\Documents\GitHub\zerog-match3\src\vapis C:\Users\darko\Documents\GitHub\zerog\lib\vapis
+mklink /D C:\Users\darko\Documents\GitHub\zerog-match3\src\vapis-em C:\Users\darko\Documents\GitHub\zerog\lib\vapis-em
+mklink C:\Users\darko\Documents\GitHub\zerog-match3\src\mt19937ar.c C:\Users\darko\Documents\GitHub\zerog\lib\mt19937ar.c
+mklink C:\Users\darko\Documents\GitHub\zerog-match3\src\mt19937ar.h C:\Users\darko\Documents\GitHub\zerog\lib\mt19937ar.h
